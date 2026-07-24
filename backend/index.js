4475e0f50f046577b5a1159bc2cc73beaaaae15a
OfirTzrik <ofirc36@gmail.com>
2026-07-24 23:17:25 +0300

    add dist to express.static

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const cors = require("cors")
 
 app.use(express.json())
 app.use(cors())
-app.use(express.static())
+app.use(express.static("dist"))
 
 morgan.token("body", (req) => JSON.stringify(req.body))
 app.use(morgan(":date[web] - :method :url :status :res[content-length] - :response-time ms :body"))

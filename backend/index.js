312d07ffd64f4c8beb85ba2d4fbd13eee2feef0a
OfirTzrik <ofirc36@gmail.com>
2026-07-24 23:12:18 +0300

    add static use

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ const cors = require("cors")
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static())
 
 morgan.token("body", (req) => JSON.stringify(req.body))
 app.use(morgan(":date[web] - :method :url :status :res[content-length] - :response-time ms :body"))

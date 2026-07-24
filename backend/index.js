c216d3ec32aee4b4da8f6663c0f1e1b6f91ce69c
OfirTzrik <ofirc36@gmail.com>
2026-07-24 23:04:07 +0300

    fix cors()

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const morgan = require("morgan")
 const app = express()
 
 app.use(express.json())
-app.use(express.cors())
+app.use(cors())
 
 morgan.token("body", (req) => JSON.stringify(req.body))
 app.use(morgan(":date[web] - :method :url :status :res[content-length] - :response-time ms :body"))

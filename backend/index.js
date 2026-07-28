cc9b2a560ce0fe9f196b4fedb5279be8275676ec
OfirTzrik <ofirc36@gmail.com>
2026-07-28 20:10:31 +0300

    fix linter errors in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,81 +1,80 @@
-require("dotenv").config()
-const express = require("express")
-const morgan = require("morgan")
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
 const app = express()
-const cors = require("cors")
-const mongoose = require("mongoose")
-const Contact = require("./models/phonebook.js")
+const cors = require('cors')
+const Contact = require('./models/phonebook.js')
 
 app.use(express.json())
 app.use(cors())
-app.use(express.static("dist"))
+app.use(express.static('dist'))
 
-morgan.token("body", (req) => JSON.stringify(req.body))
-app.use(morgan(":date[web] - :method :url :status :res[content-length] - :response-time ms :body"))
+morgan.token('body', (req) => JSON.stringify(req.body))
+app.use(morgan(':date[web] - :method :url :status :res[content-length] - :response-time ms :body'))
 
 let contacts = [
-    { 
-        "id": "1",
-        "name": "Arto Hellas", 
-        "number": "040-123456"
+    {
+        'id': '1',
+        'name': 'Arto Hellas',
+        'number': '040-123456'
     },
-    { 
-        "id": "2",
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523"
+    {
+        'id': '2',
+        'name': 'Ada Lovelace',
+        'number': '39-44-5323523'
     },
-    { 
-        "id": "3",
-        "name": "Dan Abramov", 
-        "number": "12-43-234345"
+    {
+        'id': '3',
+        'name': 'Dan Abramov',
+        'number': '12-43-234345'
     },
-    { 
-        "id": "4",
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122"
+    {
+        'id': '4',
+        'name': 'Mary Poppendieck',
+        'number': '39-23-6423122'
     }
 ]
 
-app.get("/", (req, res) => {
-    res.send("Hello World!")
+app.get('/', (req, res) => {
+    res.send('Hello World!')
 })
 
-app.get("/api/persons", (req, res) => {
+app.get('/api/persons', (req, res) => {
     Contact.find({}).then(contact => {
         res.json(contact)
     })
 })
 
-app.get("/api/persons/:id", (req, res) => {
+app.get('/api/persons/:id', (req, res) => {
     Contact.findById(req.params.id).then(contact => {
         res.json(contact)
     })
 })
 
-app.delete("/api/persons/:id", (req, res, next) => {
-    Contact.findByIdAndDelete(req.params.id).then(result => {
+app.delete('/api/persons/:id', (req, res, next) => {
+    Contact.findByIdAndDelete(req.params.id).then(() => {
         res.status(204).end()
     }).catch(error => next(error))
 })
 
-app.post("/api/persons", (req, res, next) => {
+app.post('/api/persons', (req, res, next) => {
     const body = req.body
 
-    if (!body["name"] || !body["number"]) {
+    if (!body['name'] || !body['number']) {
         return res.status(400).json({
-            error: "content missing"
+            error: 'content missing'
         })
     }
 
-    if (contacts.find(c => c.name === body["name"])) {
+    if (contacts.find(c => c.name === body['name'])) {
         return res.status(400).json({
-            error: "name already exists"
+            error: 'name already exists'
         })
     }
 
     const contact = new Contact({
-        "name": body["name"],
-        "number": body["number"]
+        'name': body['name'],
+        'number': body['number']
     })
 
     contact.save().then(savedContact => {
@@ -84,7 +83,7 @@ app.post("/api/persons", (req, res, next) => {
     }).catch(error => next(error))
 })
 
-app.put("/api/persons/:id", (req, res, next) => {
+app.put('/api/persons/:id', (req, res, next) => {
     const id = req.params.id
     const { name, number } = req.body
 
@@ -92,17 +91,17 @@ app.put("/api/persons/:id", (req, res, next) => {
         if (!contact) {
             return res.status(404).end()
         }
-        
-        contact["name"] = name
-        contact["number"] = number
-        
+
+        contact['name'] = name
+        contact['number'] = number
+
         return contact.save().then((updatedContact) => {
             res.json(updatedContact)
         })
     }).catch(error => next(error))
 })
 
-app.get("/info", (req, res, next) => {
+app.get('/info', (req, res, next) => {
     Contact.countDocuments({}).then(count => {
         res.send(`<p>Phonebook has info for ${count} people</p><p>${new Date()}</>`)
     }).catch(error => next(error))
@@ -115,7 +114,7 @@ app.listen(PORT, () => {
 
 const unknownEndpoint = (req, res) => {
     res.status(404).send({
-        error: "unknown endpoint"
+        error: 'unknown endpoint'
     })
 }
 
@@ -124,9 +123,9 @@ app.use(unknownEndpoint)
 const errorHandler = (error, request, response, next) => {
     console.error(error.message)
 
-    if (error.name === "CastError") {
-        return response.status(400).send({ error: "malformatted id" })
-    } else if (error.name === "ValidationError") {
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
         return response.status(400).json({ error: error.message })
     }
 
